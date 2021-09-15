028b072bc5aee513c70f5ee9f4f95331771010c7
SonicDarkSly <courteille.michael@gmail.com>
2021-09-16 00:37:16 +0200

    modif design

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ let container = document.getElementById("containerListe");
 fetch("http://127.0.0.1:3000/api/cameras")
     .then(response => response.json())  
     .then(response => lecture(response))
-    //.then(response2 => console.table(response2))
+
+    //.then(response2 => console.table(response2)) //affiche tableau dans console
+    
     //SI PROBLEME API
     .catch(function (err) {
         console.log("fetch Error")
@@ -15,13 +17,14 @@ fetch("http://127.0.0.1:3000/api/cameras")
 function lecture(info) {
     for (i = 0; i <= info.length; i++) { 
        container.innerHTML += `
-       <article id='${info[i]._id}' class='bg-light text-center'>
+       <article id='${info[i]._id}' class='bg-white text-center p-2 m-2 rounded border border-secondary'>
          <img src='${info[i].imageUrl}' class='img-fluid photoListe' alt='photo ${info[i].name}' />
          <div>
            <h2>${info[i].name}</h2>
            <p>${info[i].price/100}.00 €</p>
          </div>
-         <p><a href='pages/produit.html?id=${info[i]._id}'> En savoir plus</a></p>
-       </article>`
-      }
+         <p><a href='produit.html?id=${info[i]._id}'> En savoir plus</a></p>
+       </article>
+    `}
 }
+
