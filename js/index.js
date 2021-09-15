2f7a5909c908ac6de1b59deaf6a200e4e76be47e
SonicDarkSly <courteille.michael@gmail.com>
2021-09-16 01:42:21 +0200

    modif js index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 let container = document.getElementById("containerListe");
 
-
 //APPEL API AVEC FETCH
 fetch("http://127.0.0.1:3000/api/cameras")
     .then(response => response.json())  
@@ -15,16 +14,54 @@ fetch("http://127.0.0.1:3000/api/cameras")
 
 
 function lecture(info) {
-    for (i = 0; i <= info.length; i++) { 
-       container.innerHTML += `
-       <article id='${info[i]._id}' class='bg-white text-center p-2 m-2 rounded border border-secondary'>
-         <img src='${info[i].imageUrl}' class='img-fluid photoListe' alt='photo ${info[i].name}' />
-         <div>
-           <h2>${info[i].name}</h2>
-           <p>${info[i].price/100}.00 €</p>
-         </div>
-         <p><a href='produit.html?id=${info[i]._id}'> En savoir plus</a></p>
-       </article>
-    `}
+
+  for (i = 0; i <= info.length; i++) { 
+
+    let article = document.createElement('article');
+      article.setAttribute("id", info[i]._id);
+      article.setAttribute("class", "bg-white text-center p-2 m-2 rounded border border-secondary");
+
+    let img = document.createElement('img');
+      img.setAttribute("src", info[i].imageUrl);
+      img.setAttribute("class", "img-fluid photoListe"); 
+      img.setAttribute("alt", "photo de l'appareil "+info[i].name); 
+       
+    let div = document.createElement('div');
+
+    let h2 = document.createElement('h2');
+      h2.textContent = info[i].name;
+
+    let p_price = document.createElement('p');
+      p_price.textContent = info[i].price/100+".00 €";
+
+    let p_link = document.createElement('p');
+
+    let a = document.createElement('a');
+      a.setAttribute("href", "produit.html?id="+info[i]._id); 
+      a.textContent = "En savoir plus";
+
+    container.appendChild(article)
+    article.appendChild(img) 
+    article.appendChild(div)
+    div.appendChild(h2)
+    div.appendChild(p_price)
+    article.appendChild(p_link)
+    p_link.appendChild(a)
+    
+  }
+
+
+  /* for (i = 0; i <= info.length; i++) { 
+      container.innerHTML += `
+      <article id='${info[i]._id}' class='bg-white text-center p-2 m-2 rounded border border-secondary'>
+        <img src='${info[i].imageUrl}' class='img-fluid photoListe' alt='photo ${info[i].name}' />
+        <div>
+          <h2>${info[i].name}</h2>
+          <p>${info[i].price/100}.00 €</p>
+        </div>
+        <p><a href='produit.html?id=${info[i]._id}'> En savoir plus</a></p>
+      </article>
+   `} */
+
 }
 
